100169bd805f3e1be17824f49c23f8defdb8548f
KryptonFox <kirieshka0785@gmail.com>
2024-07-31 00:27:50 +0300

    Add favicon.ico and fix login error

--- components/navbar/login-button.tsx
+++ components/navbar/login-button.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/components.module.css'
 
 export default function LoginButton() {
+  const url = new URL(
+    `?client_id=${process.env.D_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(new URL('/auth', process.env.BASE_URL).toString())}&scope=identify`,
+    'https://discord.com/oauth2/authorize',
+  )
   return (
-    <a
-      href={`https://discord.com/oauth2/authorize?client_id=${process.env.D_CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&scope=identify`}
-      className={styles.loginButton}
-    >
+    <a href={url.toString()} className={styles.loginButton}>
       Войти через Discord
     </a>
   )
