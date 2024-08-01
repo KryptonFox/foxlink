fa3862c28178eb7d0c8cd6bbbd29c5d4e1a44738
KryptonFox <kirieshka0785@gmail.com>
2024-08-02 00:40:16 +0300

    delete useless PrismaClient imports

--- components/navbar/profile.tsx
+++ components/navbar/profile.tsx
@@ -1,5 +1,4 @@
 import getUserId from '@/actions/getUserId'
-import { PrismaClient } from '@prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/navbar.module.css'
