909814fb85111c9f20dd68d174acef71fb3d746d
KryptonFox <kirieshka0785@gmail.com>
2024-08-03 21:11:03 +0300

    Update /notfound page

--- app/notfound/page.tsx
+++ app/notfound/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import styles from './page.module.css'
+
+export const dynamic = 'force-static'
 
 export default function page() {
   return (
-    <main>
-      <p>Такой ссылки не существует</p>
-      <p><span>Вы всегда можете создать её </span><Link href='/'>тут</Link></p>
+    <main className={styles.container}>
+      <h1>Такой ссылки не существует</h1>
+      <p className={styles.tut}><span>Вы всегда можете создать её </span><Link href='/'>тут</Link></p>
     </main>
   )
 }
