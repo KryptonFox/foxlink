3b2944e2ed4c52a42996f81cd3b948b446b4f9e5
KryptonFox <kirieshka0785@gmail.com>
2024-08-03 21:20:44 +0300

    Add OpenGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ import Footer from '@/components/footer/footer'
 
 export const metadata: Metadata = {
   title: 'FoxLink | URL Shortener',
+  description: 'FoxLink - простой и быстрый сокращатель ссылок',
+  openGraph: {
+    title: 'FoxLink | URL Shortener',
+    description: 'FoxLink - быстрый и бесплатный сокращатель ссылок с историей переходов по ссылкам',
+    url: 'https://fxnk.ru',
+    siteName:'FoxLink',
+    type:'website',
+    locale:'ru_RU'
+  },
 }
 
 export default function RootLayout({
