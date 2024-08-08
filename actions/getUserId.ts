df5448be3ef221e881d54965b58ef9ced417e8a3
KryptonFox <kirieshka0785@gmail.com>
2024-08-08 16:08:30 +0300

    Fixed the ability to edit other`s links

--- actions/getUserId.ts
+++ actions/getUserId.ts
@@ -2,10 +2,16 @@
 
 import { jwtVerify } from 'jose'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default async function getUserId(): Promise<string> {
   // работоспособность токена проверяется в middleware
-  const secret = new TextEncoder().encode(process.env.TOKEN_SECRET)
-  const { payload } = await jwtVerify(cookies().get('token')?.value!, secret)
-  return payload.id as string
+  try {
+
+    const secret = new TextEncoder().encode(process.env.TOKEN_SECRET)
+    const { payload } = await jwtVerify(cookies().get('token')?.value!, secret)
+    return payload.id as string
+  } catch (e) {
+    redirect(new URL('/', process.env.BASE_URL!).toString())
+  }
 }
--- app/me/link/[id]/page.tsx
+++ app/me/link/[id]/page.tsx
@@ -4,11 +4,20 @@ import Loading from './loading'
 import LinkEditor from '@/components/links/link-editor'
 import LinkVisits from '@/components/links/link-visits'
 import prisma from '@/prisma/prisma'
+import getUserId from '@/actions/getUserId'
+import { redirect } from 'next/navigation'
 
 export default async function LinkPage({ params }: { params: { id: string } }) {
   const linkInfo = await prisma.link.findUnique({
     where: { id: params.id },
   })
+  // если ссылка не найдена то редирект на главную
+  if (!linkInfo)
+    redirect(new URL('/', process.env.BASE_URL!).toString())
+  // если ссылка принадлежит другому пользователю, то редирект на главную
+  if (linkInfo!.authorId !== await getUserId())
+    redirect(new URL('/', process.env.BASE_URL!).toString())
+
   return (
     <main>
       <h1 className={styles.header}>Редактор ссылки</h1>
