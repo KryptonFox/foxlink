25f5a889d7581ca500024c1897cdb204504d1633
KryptonFox <kirieshka0785@gmail.com>
2024-08-09 21:43:21 +0300

    test redirect time without record data

--- app/[link]/route.ts
+++ app/[link]/route.ts
@@ -19,31 +19,33 @@ export async function GET(
   })
   // если ссылка не существует то перекидываем на страницу ошибки
   if (!link) return NextResponse.redirect(new URL('/notfound', request.url))
-  // получение информации о том кто перешёл по ссылке
-  let visitData: VisitData = {
-    ip: request.headers.get('X-Forwarded-For')!.toString(),
-    link: { connect: { id: link.id } },
-  }
-  if (request.ip) {
-    // хостинг Vercel предоставляет информацию о геолокации
-    visitData.ip = request.ip
-    visitData.city = request.geo?.city!
-    visitData.country = request.geo?.country!
-  } else {
-    // если хостинг не Vercel то получаем данные о геолокации через ip-api.com
-    const res = await fetch(
-      `http://ip-api.com/json/${visitData.ip}?fields=status,country,city,query`,
-    )
-    const json = await res.json()
-    if (json.status === 'success') {
-      visitData.ip = json.query
-      visitData.city = json.city
-      visitData.country = json.country
-    }
-  }
-  // запись данных о посещении в бд
-  await prisma.visit.create({ data: visitData })
-
-  // перекидывание на страницу
+    
   return NextResponse.redirect(link.url)
+  // // получение информации о том кто перешёл по ссылке
+  // let visitData: VisitData = {
+  //   ip: request.headers.get('X-Forwarded-For')!.toString(),
+  //   link: { connect: { id: link.id } },
+  // }
+  // if (request.ip) {
+  //   // хостинг Vercel предоставляет информацию о геолокации
+  //   visitData.ip = request.ip
+  //   visitData.city = request.geo?.city!
+  //   visitData.country = request.geo?.country!
+  // } else {
+  //   // если хостинг не Vercel то получаем данные о геолокации через ip-api.com
+  //   const res = await fetch(
+  //     `http://ip-api.com/json/${visitData.ip}?fields=status,country,city,query`,
+  //   )
+  //   const json = await res.json()
+  //   if (json.status === 'success') {
+  //     visitData.ip = json.query
+  //     visitData.city = json.city
+  //     visitData.country = json.country
+  //   }
+  // }
+  // // запись данных о посещении в бд
+  // await prisma.visit.create({ data: visitData })
+
+  // // перекидывание на страницу
+  // return NextResponse.redirect(link.url)
 }
